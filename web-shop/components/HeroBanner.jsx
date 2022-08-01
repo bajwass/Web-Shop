384ffbb98afbee4d587293bdd67d5538565629ee
Sumeet <sumeet-bajwa-34@live.ca>
2022-07-31 21:36:16 -0700

    HeroBanner
    HeroBanner is now fully coded into the homepage
    
    Keyboard image and all the text now showing

--- web-shop/components/HeroBanner.jsx
+++ web-shop/components/HeroBanner.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
+import { urlFor } from '../lib/client'
 
-const HeroBanner = () => {
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='g915'>SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src="" alt="keyboard" className='hero-banner-image'/>
+        <p className='g915'>{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <h1>{heroBanner.largeText2}</h1>
+        
+        <img src={urlFor(heroBanner.image)} alt="keyboard" className='hero-banner-image'/>
         <div>
 
         </div>
       </div>
-      <Link href="/product/ID">
+      <Link href={`/product/${heroBanner.product}`}>
 
-        <button type="button">BUTTON TEXT</button>
+        <button type="button">{heroBanner.buttonText}</button>
 
       </Link>
       <div className='desc'>
-        <h5>Description</h5>
-        <p>DESCRIPTION</p>
+        <h5>{heroBanner.desc}</h5>
+        <p>{heroBanner.smallText}</p>
 
       </div>
       
