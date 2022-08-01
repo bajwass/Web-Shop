dfee312d911aac4aa9c8d8dff55748c85ca24427
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-01 16:46:10 -0700

    Update _app.js

--- web-shop/pages/_app.js
+++ web-shop/pages/_app.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import { Layout } from '../components'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Layout } from '../components';
 import { StateContext } from '../context/StateContext';
-import '../styles/globals.css'
+import '../styles/globals.css';
+
 
 
 function MyApp({ Component, pageProps }) {
   return(
     <StateContext>
       <Layout>
-
+        <Toaster />
         <Component {...pageProps} />
 
       </Layout>
