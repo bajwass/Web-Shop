114d0006ca155ffe0e2a1a4f80cfea663b175482
Sumeet <sumeet-bajwa-34@live.ca>
2022-07-31 19:40:53 -0700

    Update index.js
    Initial setup of the home page,
    
    with banner, titles and where the products will be fetched

---
+++ web-shop/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    HeroBanner
+
+    <div>
+      <h2>Best Selling Products</h2>
+      <p>All types of Gaming Keyboards</p>
+    </div>
+    <div>
+      {['Product 1', 'Product 2'].map((product) => product)}
+    </div>
+    </>
+  )
+}
+
+export default Home;
