82577569c294d82e7a909af4066d57797804f458
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-01 20:06:56 -0700

    Remove
    Can now completely delete items from the cart

--- web-shop/context/StateContext.js
+++ web-shop/context/StateContext.js
@@ -37,6 +37,15 @@ export const StateContext = ({children}) => {
             setCartItems([...cartItems, { ...product }]);
         }
         toast.success(`${qty} ${product.name} added to the cart.`);
+
+    }
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === product._id);
+        const newCartItems = cartItems.filter((item) => item._id !== product._id);
+    
+        setTotalPrice((prevTotalPrice) => prevTotalPrice -foundProduct.price * foundProduct.quantity);
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity);
+        setCartItems(newCartItems);
     }
     const toggleCartItemQuantity = (id,value) => {
         foundProduct = cartItems.find((item) => item._id === id);
@@ -82,7 +91,8 @@ export const StateContext = ({children}) => {
                 incQty,
                 decQty,
                 onAdd,
-                toggleCartItemQuantity
+                toggleCartItemQuantity,
+                onRemove
             }}
         >
             {children}
