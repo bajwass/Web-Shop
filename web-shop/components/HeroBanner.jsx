1e61a12dba22ef17f6651a20ef6a4e16f69d44ad
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-02 15:18:48 -0700

    Update HeroBanner.jsx

--- web-shop/components/HeroBanner.jsx
+++ web-shop/components/HeroBanner.jsx
@@ -1,35 +1,28 @@
-import React from 'react'
-import Link from 'next/link'
-import { urlFor } from '../lib/client'
+import React from 'react';
+import Link from 'next/link';
 
-const HeroBanner = ({heroBanner}) => {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className='hero-banner-container'>
+    <div className="hero-banner-container">
       <div>
-        <p className='g915'>{heroBanner.smallText}</p>
+        <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
         <h1>{heroBanner.largeText2}</h1>
-        
-        <img src={urlFor(heroBanner.image)} alt="keyboard" className='hero-banner-image'
-        />
-        <div>
+        <img src={urlFor(heroBanner.image)} alt="keyboard" className="hero-banner-image" />
 
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
         </div>
       </div>
-      <Link href={`/product/${heroBanner.product}`}>
-
-        <button type="button">{heroBanner.buttonText}</button>
-
-      </Link>
-      <div className='desc'>
-        <h5>{heroBanner.desc}</h5>
-        <p>{heroBanner.smallText}</p>
-
-      </div>
-      
-
-
     </div>
   )
 }
