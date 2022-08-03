f33b9cd01f38c0fa23cf2959baac98d869451402
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-02 18:30:16 -0700

    mini updates

--- web-shop/components/Footer.jsx
+++ web-shop/components/Footer.jsx
@@ -5,7 +5,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2022 Sumeet's Keyboard All Rights Reserved</p>
+      <p>2022 Sumeet's Keyboard's All Rights Reserved</p>
       <p className='icons'>
         <AiFillInstagram />
         <AiOutlineTwitter />
