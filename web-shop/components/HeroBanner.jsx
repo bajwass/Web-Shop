5a04cf2c7c5b63eef18593b999a1b552d648a4b5
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-02 22:32:14 -0700

    minor updates

--- web-shop/components/HeroBanner.jsx
+++ web-shop/components/HeroBanner.jsx
@@ -10,16 +10,17 @@ const HeroBanner = ({ heroBanner }) => {
         <p className="g915">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
+        <h1>{heroBanner.largeText2}</h1>
         <img src={urlFor(heroBanner.image)} alt="keyboard" className="hero-banner-image" />
 
         <div>
           <Link href={`/product/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
-          <div className="desc">
+          {/* <div className="desc">
             <h5>Description</h5>
             <p>{heroBanner.desc}</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
