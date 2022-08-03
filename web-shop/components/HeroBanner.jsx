9d0e874eca36a81c10bdde6d22aab5946508e319
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-02 18:50:17 -0700

    Text updates

--- web-shop/components/HeroBanner.jsx
+++ web-shop/components/HeroBanner.jsx
@@ -10,7 +10,6 @@ const HeroBanner = ({ heroBanner }) => {
         <p className="g915">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <h1>{heroBanner.largeText2}</h1>
         <img src={urlFor(heroBanner.image)} alt="keyboard" className="hero-banner-image" />
 
         <div>
