7e8896104f21cde65cf2f92e681d3e9ddafa34ea
Sumeet <sumeet-bajwa-34@live.ca>
2023-03-02 13:18:42 -0800

    Update success.js

--- web-shop/pages/success.js
+++ web-shop/pages/success.js
@@ -26,8 +26,8 @@ const Success = () => {
         <p className="email-msg">Check your email inbox for the receipt.</p>
         <p className="description">
           If you have any questions, please email
-          <a className="email" href="mailto:order@example.com">
-            order@example.com
+          <a className="email" href="sumeet-bajwa-34@live.ca">
+            sumeet-bajwa-34@live.ca
           </a>
         </p>
         <Link href="/">
